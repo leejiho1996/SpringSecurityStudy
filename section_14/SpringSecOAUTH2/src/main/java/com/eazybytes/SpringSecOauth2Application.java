5ebd41923c3667d6cc3c426fb1efd17ee33b9f43
이지호 <leech2479@naver.com>
2025-01-10 00:51:37 +0900

    section14 start

---
+++ section_14/SpringSecOAUTH2/src/main/java/com/eazybytes/SpringSecOauth2Application.java
@@ -0,0 +1,13 @@
+package com.eazybytes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecOauth2Application.class, args);
+	}
+
+}
